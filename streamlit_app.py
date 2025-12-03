c8b8579b86e7a91fb9f252a61aeb127f42e6e796
crmastersPHP <casandra.masters@providence.org>
2025-12-03 14:00:16 -0800

    Update streamlit_app.py
    Change the streamlit.title()

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-  """Choose the fruits you want in your custom Smoothie!
-  """
-)
+st.title("My Parents New Healthy Diner")
 
 # Add name box
 name_on_order = st.text_input("Name on Smoothie:")
