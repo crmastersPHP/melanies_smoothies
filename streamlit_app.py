68b81791d79e7fe59d561f7dbfd44dd0a568c0da
crmastersPHP <casandra.masters@providence.org>
2025-12-03 14:21:07 -0800

    Update streamlit_app.py
    change back to original smoothie form

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,15 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("My Parents New Healthy Diner")
-st.subheader(f"Breakfast Menu")
-st.write(""" Omega 3 & Blueberry Oatmeal\n
-    Kale, Spinach & Rocket Smoothie\n
-    Hard-Boiled Free-Range Egg
-    """)
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
+)
 
 # Add name box
-#name_on_order = st.text_input("Name on Smoothie:")
-#st.write("The name on your smoothie will be:", name_on_order)
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
